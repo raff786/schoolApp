86b8973d5364e0b0f5e0116f1ea67735e1b66e01
Shujaha61290 <Shujaha61290@GB-LD1LPW10G007.mastek.com>
2020-03-18 11:11:42 +0000

    updated students entity

--- schoolApp/src/main/java/com/mastek/schoolApp/entities/Students.java
+++ schoolApp/src/main/java/com/mastek/schoolApp/entities/Students.java
@@ -1,13 +1,97 @@
 package com.mastek.schoolApp.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="JPA_Students")
 public class Students {
  
 	int studentId;
 	String studentName;
-	gender gender;
+	Gender gender;
 	String studentEmail;
 	String dateOfBirth;
 	String studentAddress;
 	String studentEmergencyContact;
 	boolean studentLearningDifficulty;
+	
+	
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public Gender getGender() {
+		return gender;
+	}
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	public String getStudentEmail() {
+		return studentEmail;
+	}
+	public void setStudentEmail(String studentEmail) {
+		this.studentEmail = studentEmail;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+	public void setStudentAddress(String studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+	public String getStudentEmergencyContact() {
+		return studentEmergencyContact;
+	}
+	public void setStudentEmergencyContact(String studentEmergencyContact) {
+		this.studentEmergencyContact = studentEmergencyContact;
+	}
+	public boolean isStudentLearningDifficulty() {
+		return studentLearningDifficulty;
+	}
+	public void setStudentLearningDifficulty(boolean studentLearningDifficulty) {
+		this.studentLearningDifficulty = studentLearningDifficulty;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + studentId;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Students other = (Students) obj;
+		if (studentId != other.studentId)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Students [studentId=" + studentId + ", studentName=" + studentName + ", gender=" + gender
+				+ ", studentEmail=" + studentEmail + ", dateOfBirth=" + dateOfBirth + ", studentAddress="
+				+ studentAddress + ", studentEmergencyContact=" + studentEmergencyContact
+				+ ", studentLearningDifficulty=" + studentLearningDifficulty + "]";
+	}
+	
+	
 }
